d5d9061eef43591cd733df2a688de9a97c498ae4
Your Name <you@example.com>
2022-06-26 18:13:05 +0300

    update

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ import cookieParser from 'cookie-parser';
 const app = express();
 const port = 3000;
 
-const ACCOUNTS_MOCK = {
-    admin: {
-        id: 1,
-        login: 'admin',
-        password: '1234'
-    }
-}
+const ACCOUNTS_MOCK = [
+  {
+    id: 1,
+    login: 'admin',
+    password: '1234'
+  }
+];
 
-const sessions = new Map; // map sign in cookie -> session
+const sessions = new Map(); // map sign in cookie -> session
 
 // Parse cookies
 app.use(cookieParser());
@@ -23,88 +23,96 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+app.get('/home.html', (req, res, next) => {
+  if (sessions.get(req.cookies.sign)) {
+    next();
+  } else {
+    res.redirect(300, '/');
+  }
+});
+
 // Serve static files
 app.use(express.static('static'));
 
-// Account CRUD 
+// Account CRUD
 app.post('/account/get', (req, res) => {
-    console.log('Account found');
-    // res.send(JSON.stringify(account));
-    res.end(200);
+  console.log('Account found');
+  // res.send(JSON.stringify(account));
+  res.status(200);
+  res.end();
 });
 
 app.post('/account/delete', (req, res) => {
-    // Deleting account
-    console.log('Account deleted');
-    res.end(200);
+  // Deleting account
+  console.log('Account deleted');
+  res.status(200);
+  res.end();
 });
 
 app.post('/account/update', (req, res) => {
-    // Updating account
-    console.log('Account updated');
-    res.end(200);
+  // Updating account
+  console.log('Account updated');
+  res.status(200);
+  res.end();
 });
 
 app.post('/account/create', (req, res) => {
-    // Creating account
-    console.log('Account created');
-    res.end(200);
+  // Creating account
+  console.log('Account created');
+  res.status(200);
+  res.end();
 });
 
 app.post('/account/login', (req, res) => {
-    const account = Object.keys(ACCOUNTS_MOCK).find(login => login === req.body.login);
-    if (account) {
-        if (req.body.password == account.password) {
+  const account = ACCOUNTS_MOCK.find(({ login }) => login === req.body.login);
+  if (account && req.body.password === account.password) {
+    let cookie = Math.random().toString();
+    cookie = cookie.substring(2, cookie.length);
+    res.cookie('sign', cookie, { maxAge: 900000, httpOnly: false });
 
-            let cookie = Math.random().toString();
-            cookie = cookie.substring(2, cookie.length);
-            res.cookie('sign', cookie, { maxAge: 900000, httpOnly: true });
+    sessions.set(cookie, account);
 
-            sessions.set(account.id, cookie);
-
-            console.log('Log in success');
-            res.redirect(307, '/home.html');
-        }
-    }
-
+    console.log('Log in success');
+    res.redirect(302, '/home.html');
+    res.end();
+  } else {
     console.log('Log in failed');
-    res.end(403);
+    res.status(403);
+    res.end();
+  }
 });
 
-// Animal CRUD 
+// Animal CRUD
 app.post('/animal/get', (req, res) => {
-    console.log('Animal found');
-    // res.send(JSON.stringify(account));
-    res.end(200);
+  console.log('Animal found');
+  // res.send(JSON.stringify(account));
+  res.status(200);
+  res.end();
 });
 
 app.post('/animal/delete', (req, res) => {
-    // Deleting animal
-    console.log('Animal deleted');
-    res.end(200);
+  // Deleting animal
+  console.log('Animal deleted');
+  res.status(200);
+  res.end();
 });
 
 app.post('/animal/update', (req, res) => {
-    // Updating animal
-    console.log('Animal updated');
-    res.end(200);
+  // Updating animal
+  console.log('Animal updated');
+  res.status(200);
+  res.end();
 });
 
 app.post('/animal/create', (req, res) => {
-    // Creating animal
-    console.log('Animal created');
-    res.end(200);
+  // Creating animal
+  console.log('Animal created');
+  res.status(200);
+  res.end();
 });
 
-app.use('/home.html', (req, res) => {
-    if (sessions.get(req.cookies.sign)) {
-        next();
-    } else {
-        res.redirect(403, '/');
-    }
-});
 
 app.listen(port, () => {
-    console.log(`Http server listening on http://localhost:${port}`)
-})
+  console.log(`Http server listening on http://localhost:${port}`);
+});
 
